186c79daf889146ffc243c442584b4a1826e8936
Taha Aoui Amar <taha.aoui.amar@hva.nl>
2023-03-27 21:46:37 +0200

    First push

---
+++ static/style.css
@@ -0,0 +1,38 @@
+body {
+	margin: 0;
+	padding: 0;
+	font-family: Arial, sans-serif;
+}
+
+.container {
+	max-width: 600px;
+	margin: 0 auto;
+	padding: 50px 20px;
+}
+
+h1 {
+	font-size: 36px;
+	margin-bottom: 20px;
+}
+
+p {
+	font-size: 18px;
+	margin-bottom: 20px;
+}
+
+input[type="text"] {
+	padding: 10px 15px;
+	border-radius: 5px;
+	border: none;
+	background-color: #f7f7f7;
+	margin-right: 10px;
+}
+
+input[type="submit"] {
+	padding: 10px 15px;
+	border-radius: 5px;
+	border: none;
+	background-color: #2196F3;
+	color: #fff;
+	cursor: pointer;
+}
